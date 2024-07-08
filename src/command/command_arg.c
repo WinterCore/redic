646449aeac4156f18de4ab67e83b1d7a5ed8a06f
WinterCore <hogobbl@gmail.com>
2024-07-08 21:37:07 +0400

    fix: Initial work on supporting unordered args that has tokens

--- src/command/command_arg.c
+++ src/command/command_arg.c
@@ -2,13 +2,47 @@
 
 #include "./command.h"
 
+typedef struct CommandArguments {
+    RESPBulkString **args;
+    size_t len;
+    size_t pos;
+} CommandArguments;
+
+CommandArguments command_arguments_create(size_t len, RESPBulkString **input_args) {
+    CommandArguments cmd_args = {
+        .args = input_args,
+        .len = len,
+        .pos = 0,
+    };
+
+    return cmd_args;
+}
+
+RESPBulkString *command_arguments_consume(CommandArguments *cmd_args) {
+    if (cmd_args->pos == cmd_args->len) {
+        return NULL;
+    }
+
+    return cmd_args->args[cmd_args->pos++];
+}
+
 CommandArgumentsParseResult parse_command_argument(
     Arena *arena,
     CommandArgDefinition *arg_definition,
-    RESPBulkString **input_args,
-    CommandArg **output_command_arg,
-    size_t *consumed_args
+    CommandArguments *cmd_args,
+    CommandArg **output_command_arg
 ) {
+    // TODO: If the argument has a token we need to consume to input arguments
+    RESPBulkString *token_arg = NULL;
+
+    // Token is required but there's no more input args
+    if (arg_definition->token != NULL) {
+        if ((token_arg = command_arguments_consume(cmd_args)) == NULL) {
+            return CMD_ARGS_TOO_FEW_ARGS;
+        }
+    }
+
+
     // Early error return; Field is required and there's no value
     if (! arg_definition->is_optional && input_arg == NULL) {
         return CMD_ARGS_TOO_FEW_ARGS;                    
@@ -66,29 +100,43 @@ CommandArgumentsParseResult parse_command_arguments(
     RESPBulkString **input_args,
     CommandArg **output_command_args
 ) {
+    CommandArguments cmd_args = command_arguments_create(input_args_len, input_args);
+
     // TODO: Handle too many arguments
-    for (size_t i = 0; i < arg_definitions_len;) {
+    // TODO: Flip the logic here and iterate over arg definitions instead
+    for (size_t i = 0; i < arg_definitions_len; i += 1) {
         CommandArgDefinition def = arg_definitions[i];
         RESPBulkString *input_arg = i < input_args_len
             ? input_args[i]
             : NULL;
 
-        size_t consumed_args = 0;
 
-        // TODO: finish this
+        // TODO: If the argument is a token then search and destroy
         CommandArgumentsParseResult result = parse_command_argument(
             arena,
             &def,
-            input_args,
-            &output_command_args[i],
-            &consumed_args
+            &cmd_args,
+            &output_command_args[i]
         ); 
-
-        i += consumed_args; 
 
         if (result != CMD_ARGS_PARSE_SUCCESS) {
             return result;
         }
+    }
+
+
+    // GET THE NUMBER OF REQUIRED ARGUMENTS
+    size_t num_args_required = 0;
+
+    for (size_t i = 0; i < arg_definitions_len; i += 1) {
+        if (! arg_definitions[i].is_optional) {
+            num_args_required += 1;
+        }
+    }
+
+    while (cmd_args.pos < cmd_args.len) {
+        // Iterate over all required arguments in arg_definitions and make sure we fulfill them first
+        
     }
 
     return CMD_ARGS_PARSE_SUCCESS;
